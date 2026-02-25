5aacd98a811d11edf7b5b8ade30f35776a43840a
Andrey Horsev <andrey.horsev@gmail.com>
2026-02-25 12:22:31 +0200

    Fix key generation logic in attachRandom function to ensure proper range for randomInt

--- src/generateKey.js
+++ src/generateKey.js
@@ -3,7 +3,7 @@ import { randomInt } from "node:crypto";
 const SAFE_ALPHABET = "ABCDEFGHIJKLMNPQRSTUVWXYZ123456789";
 
 const toArray = (str) => str.split("");
-const attachRandom = (val) => ({ val, key: randomInt(2 ** 48) });
+const attachRandom = (val) => ({ val, key: randomInt(2 ** 48 - 1) });
 const compareByKey = (a, b) => a.key - b.key;
 const extractVal = ({ val }) => val;
 
